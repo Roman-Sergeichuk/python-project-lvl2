1b2cea6e109f80a4a06e1ef9f1bbdfae6027e1e8
Sergeichuk Roman <sergeichuk.roman@mail.ru>
2020-06-09 22:41:02 +0300

    correction of generate_nested format function

--- gendiff/format/default.py
+++ gendiff/format/default.py
@@ -16,7 +16,8 @@ def format_complex_value(complex_value, prefix, indent):
     return result
 
 
-def iterate_data(common_data, out_string, indent):
+def generate_nested_format(common_data, *, indent='    '):
+    out_string = '{'
     prefix_add = '+ '
     prefix_remove = '- '
     prefix_non_changed = '  '
@@ -24,7 +25,7 @@ def iterate_data(common_data, out_string, indent):
     for key, (status, value) in sorted(common_data.items()):
         if status == diff.NESTED:
             out_string += f'\n{indent}{key}: ' \
-                          f'{iterate_data(value, out_string="{", indent = indent + tab)}'  # noqa: E501
+                          f'{generate_nested_format(value, indent=indent + tab)}'  # noqa: E501
         elif status == diff.EQUAL:
             out_string += f'\n{indent}{prefix_non_changed}{key}: {value}'
         elif status == diff.CHANGED:
@@ -34,7 +35,7 @@ def iterate_data(common_data, out_string, indent):
         elif status == diff.ADDED:
             if isinstance(value, dict):
                 out_string += f'\n{indent}{prefix_add}{key}: ' \
-                              f'{format_complex_value(value, prefix=prefix_non_changed, indent=indent+tab)}'  # noqa: E501
+                              f'{format_complex_value(value, prefix=prefix_non_changed, indent=indent + tab)}'  # noqa: E501
             else:
                 out_string += f'\n{indent}{prefix_add}{key}: {value}'
         elif status == diff.REMOVED:
@@ -48,9 +49,3 @@ def iterate_data(common_data, out_string, indent):
         out_string += '\n'
     out_string += indent + '}'
     return out_string
-
-
-def generate_nested_format(common_data):
-    out_string = '{'
-    indent = '    '
-    return iterate_data(common_data, out_string, indent)
